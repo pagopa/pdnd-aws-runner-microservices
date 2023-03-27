a6a38e65568d8ebf28ab903c1e25be2edc2c6bf5
mronci <112618063+mronci@users.noreply.github.com>
2023-03-27 09:32:27 +0200

    fix(ci): splitting nexus secret

--- .identity/03_github_environment_ci.tf
+++ .identity/03_github_environment_ci.tf
@@ -3,37 +3,37 @@
 resource "github_actions_secret" "github_token" {
   repository       = var.repo_name
   secret_name      = "PAT_BOT"
-  plaintext_value  = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["PAT_BOT"]
+  plaintext_value  = jsondecode(data.aws_secretsmanager_secret_version.github_current.secret_string)["PAT_BOT"]
 }
 
 resource "github_actions_secret" "ovpn_username" {
   repository       = var.repo_name
   secret_name      = "OVPN_USERNAME"
-  plaintext_value = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["OVPN_USERNAME"]
+  plaintext_value = jsondecode(data.aws_secretsmanager_secret_version.github_current.secret_string)["OVPN_USERNAME"]
 }
 
 resource "github_actions_secret" "ovpn_client_key" {
   repository       = var.repo_name
   secret_name      = "OVPN_CLIENT_KEY"
-  plaintext_value  = base64decode(jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["OVPN_CLIENT_KEY"])
+  plaintext_value  = base64decode(jsondecode(data.aws_secretsmanager_secret_version.github_current.secret_string)["OVPN_CLIENT_KEY"])
 }
 
 resource "github_actions_secret" "ovpn_tls_auth_key" {
   repository       = var.repo_name
   secret_name      = "OVPN_TLS_AUTH_KEY"
-  plaintext_value  = base64decode(jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["OVPN_TLS_AUTH_KEY"])
+  plaintext_value  = base64decode(jsondecode(data.aws_secretsmanager_secret_version.github_current.secret_string)["OVPN_TLS_AUTH_KEY"])
 }
 
 resource "github_actions_secret" "nexus_username" {
   repository       = var.repo_name
   secret_name      = "NEXUS_USERNAME"
-  plaintext_value  = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["NEXUS_USERNAME"]
+  plaintext_value  = jsondecode(data.aws_secretsmanager_secret_version.nexus_credentials_current.secret_string)["username"]
 }
 
 resource "github_actions_secret" "nexus_password" {
   repository       = var.repo_name
   secret_name      = "NEXUS_PASSWORD"
-  plaintext_value  = jsondecode(data.aws_secretsmanager_secret_version.current.secret_string)["NEXUS_PASSWORD"]
+  plaintext_value  = jsondecode(data.aws_secretsmanager_secret_version.nexus_credentials_current.secret_string)["password"]
 }
 
 resource "github_actions_variable" "image_registry_url" {
